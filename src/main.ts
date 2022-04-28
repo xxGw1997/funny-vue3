f9c9b0da43cc08f3aec56b9565061252fd5cdef9
xxgw <wenxuan.yang@dragonsoftbravo.com>
2022-04-28 18:21:20 +0800

    添加暗黑模式

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import "@/styles/main.css"
+
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 const pinia = createPinia()
+
 
 
 import 'virtual:windi.css'
